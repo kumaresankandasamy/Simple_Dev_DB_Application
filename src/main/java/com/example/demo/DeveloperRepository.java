2138a5852ee463fa2c95d0d1f7668667aba81669
unknown <kumaresank2000@gmail.com>
2019-09-19 17:04:33 +0530

    add controllers and templates

--- src/main/java/com/example/demo/DeveloperRepository.java
+++ src/main/java/com/example/demo/DeveloperRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface DeveloperRepository extends CrudRepository<Developer,Long>{
 
+	Developer findOne(long skillId);
+
 }
--- src/main/java/com/example/demo/SkillRepository.java
+++ src/main/java/com/example/demo/SkillRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface SkillRepository extends CrudRepository<Skill,Long> {
 	public List<Skill> findByLabel(String label);
 
+	public Skill findOne(long skillId);
+
 }
